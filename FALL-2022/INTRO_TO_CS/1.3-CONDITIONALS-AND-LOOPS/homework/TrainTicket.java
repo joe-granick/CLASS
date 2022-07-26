9eedc20ec222923a2421542cba3d6d7c53576680
jgranick <jmgranick@gmail.com>
2022-07-26 17:42:24 -0400

    Homework 1.3 Problem 2 TrainTicket.java

---
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/homework/TrainTicket.java
@@ -0,0 +1,17 @@
+public class TrainTicket {
+    public static void main(String[] args){
+
+        int age = Integer.parseInt(args[0]);
+        boolean purchasedAtStation = Boolean.parseBoolean(args[1]);
+        double price = 0.0;
+        if (age < 120 && age >0){
+            if(age>65) price = 7.50;
+            else if(age >=7) price = 13.20;
+            if(!purchasedAtStation) price = price*1.20;
+            System.out.print("$"+(double)Math.round(price*100.0)/100.0);
+        }
+        else System.out.print("Illegal input");
+
+    }
+    
+}
