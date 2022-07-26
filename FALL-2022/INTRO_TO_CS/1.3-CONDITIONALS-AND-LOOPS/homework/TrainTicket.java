8dfcc3b8f298fa7fdbe2fcfb5eeb6973f4dcc78b
jgranick <jmgranick@gmail.com>
2022-07-26 17:48:18 -0400

    Added comment about Math.round HW 1.3 Problem 2 TrainTicket

--- FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/homework/TrainTicket.java
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/homework/TrainTicket.java
@@ -8,7 +8,8 @@ public class TrainTicket {
             if(age>65) price = 7.50;
             else if(age >=7) price = 13.20;
             if(!purchasedAtStation) price = price*1.20;
-            System.out.print("$"+(double)Math.round(price*100.0)/100.0);
+            //Math.round() retunrs long integer type, need to multiply by 100 and cast to convert
+            System.out.print("$"+(double)Math.round(price*100.0)/100.0); 
         }
         else System.out.print("Illegal input");
 
