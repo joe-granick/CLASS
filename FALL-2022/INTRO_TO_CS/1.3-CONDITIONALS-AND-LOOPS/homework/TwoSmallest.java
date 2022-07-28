3a7e5c3dc36dcbbf1e1ae53c3e738f4130f4594a
jgranick <jmgranick@gmail.com>
2022-07-28 09:11:28 -0400

    Homework 1.3 Problem 5 TwoSmallest.java

---
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/homework/TwoSmallest.java
@@ -0,0 +1,20 @@
+public class TwoSmallest {
+    public static void main(String[] args){
+        int count = args.length;
+        int i = 0;
+        double min_val = Double.MAX_VALUE;
+        double next_min = Double.MAX_VALUE;
+        double check_val;
+        while(i<count){
+            check_val = Double.parseDouble(args[i]);
+            if(min_val >= check_val){ //>= so that next min will be assigned when there is a duplicate min val
+                next_min = min_val;
+                min_val = check_val;
+            } 
+            i++;
+        }
+        System.out.println(min_val);
+        System.out.println(next_min);
+    }
+    
+}
