1584a75cfb9b9daced1099c258f883406e6d0237
jgranick <jmgranick@gmail.com>
2022-08-01 12:11:14 -0400

    1.3 pg 63 Nested Loops Example Program 1.3.4 DivisorPattern.java

--- FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXAMPLES/PowerTwoLessThan.java
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXAMPLES/PowerTwoLessThan.java
@@ -1,15 +1,11 @@
 public class PowerTwoLessThan {
     public static void main(String[] args){
+
         int n = Integer.parseInt(args[0]);
         int result = 1;
         while(result <= n/2){
             result = 2*result;
-            System.out.println("Sum: " + result);
-
         }
-
         System.out.println(result);
-
-    }
-    
+    }    
 }
