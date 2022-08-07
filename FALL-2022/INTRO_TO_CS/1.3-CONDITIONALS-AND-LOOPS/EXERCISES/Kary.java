1e2f98def3cca87afe9fe1cfa1dcd74114cf6205
jgranick <jmgranick@gmail.com>
2022-08-07 09:09:50 -0400

    folder change

--- FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/Kary.java
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/Kary.java
@@ -2,22 +2,24 @@ public class Kary {
     public static void main(String[] args){
         int n = Integer.parseInt(args[0]);
         int k = Integer.parseInt(args[1]);
-        int multiple = k-1;
         int bit = 1;
         int i = 0;
-        String code = "";
+
 
         while(bit <= n/k){
             i++;
             bit*=k;
             System.out.println("value: "+ bit + " power: " + i);
-        } 
+        }
+
+        String code = "";
+        int multiple = k-1; 
         while (bit > 0){
                 while (n < multiple*bit){
                     multiple--;
                     }
-                code += multiple;
-            
+
+            code += multiple;    
             n = n-multiple*bit;
             bit = bit/k;
             multiple = k-1; //since it's an int 1/2 rounds to 0
