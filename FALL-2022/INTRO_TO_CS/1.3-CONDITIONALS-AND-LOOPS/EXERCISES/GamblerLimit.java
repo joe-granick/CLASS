05d93da729946918176c369bd0638c6e75448e0b
jgranick <jmgranick@gmail.com>
2022-08-08 16:49:31 -0400

    exercise 1.3.26 Gambler limit pg 84

---
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/GamblerLimit.java
@@ -0,0 +1,27 @@
+public class GamblerLimit {
+        public static void main(String[] args){
+            int stake = Integer.parseInt(args[0]);
+            int goal = Integer.parseInt(args[1]);
+            int trials = Integer.parseInt(args[2]);
+            int l = Integer.parseInt(args[3]);
+            int bets = 0;
+            int wins = 0;
+            
+    
+            for(int t = 0; t < trials; t++){
+                int cash = stake;
+                int plays = 0;
+                while (cash > 0 && cash < goal && plays < l){
+                    bets++;
+                    if (Math.random() < 0.5) cash++;
+                    else                   cash--;
+                }
+                if (cash == goal) wins++;
+                System.out.println("Walkaway $" + cash);
+            }
+            System.out.println(100*wins/trials+ "% wins");
+            System.out.println("Avg # bets: " + (double)bets/(double)trials);
+            //System.out.println("Walkaway: " + cash);
+        }
+    }
+    
