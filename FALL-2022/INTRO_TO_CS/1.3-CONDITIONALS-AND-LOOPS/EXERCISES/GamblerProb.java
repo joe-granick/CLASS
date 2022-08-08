7c20eba25634c7a092b4110200a7c4f7d6398e17
jgranick <jmgranick@gmail.com>
2022-08-08 16:35:58 -0400

    exercise 1.3.26 Gambler Probability pg 84

---
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/GamblerProb.java
@@ -0,0 +1,23 @@
+public class GamblerProb {
+        public static void main(String[] args){
+            int stake = Integer.parseInt(args[0]);
+            int goal = Integer.parseInt(args[1]);
+            int trials = Integer.parseInt(args[2]);
+            double p = Double.parseDouble(args[3]);
+            int bets = 0;
+            int wins = 0;
+    
+            for(int t = 0; t < trials; t++){
+                int cash = stake;
+                while (cash > 0 && cash < goal){
+                    bets++;
+                    if (Math.random() < p) cash++;
+                    else                   cash--;
+                }
+                if (cash == goal) wins++;
+            }
+            System.out.println(100*wins/trials + "% wins");
+            System.out.println("Avg # bets: " + (double)bets/(double)trials);
+            System.out.println("Total:"+trials + " W:" + wins+ " L:" + (trials - wins));
+        }
+}
