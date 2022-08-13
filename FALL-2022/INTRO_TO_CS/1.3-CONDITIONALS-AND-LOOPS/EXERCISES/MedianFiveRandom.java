000cbb3a982a7c2b621fbf6c5ac25ae795267950
jgranick <jmgranick@gmail.com>
2022-08-13 12:56:17 -0400

    1.3.443 pg 89 5 Number Median Solved, now try to get below 7 compairsons

--- FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/MedianFiveRandom.java
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/MedianFiveRandom.java
@@ -8,53 +8,73 @@ public class MedianFiveRandom {
     int e = Integer.parseInt(args[4]);
     */
     int n = Integer.parseInt(args[0]);
+    int incorrect = 0;
     for(int i = 0; i < n; i++ ){
-    
-    int t;
-    int a = (int)(Math.random()*100.0);
-    int b = (int)(Math.random()*100.0);
-    int c = (int)(Math.random()*100.0);
-    int d = (int)(Math.random()*100.0);
-    int e = (int)(Math.random()*100.0);
+        boolean pass = false;
+        int t;
+        int a = (int)(Math.random()*1000.0);
+        int b = (int)(Math.random()*1000.0);
+        int c = (int)(Math.random()*1000.0);
+        int d = (int)(Math.random()*1000.0);
+        int e = (int)(Math.random()*1000.0);
+        
 
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (a > b) {
-        t = a;
-        a = b;
-        b = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (a > c) {
-        t = a;
-        a = c;
-        c = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (b > c) {
-        t = c;
-        c = b;
-        b = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (e < d) {
-        t = e;
-        e = d;
-        d = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (e < c) {
-        t = e;
-        e = c;
-        c = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (d < c) {
-        t = d;
-        d = c;
-        c = t;
-    }
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        
 
-    System.out.println("Median: " + c + " order: " + a + "," + b + "," + c +"," + d + "," + e );
-}
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c < a) {
+            t = a;
+            a = c;
+            c = t;
+        }
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c > e) {
+            t = e;
+            e = c;
+            c = t;
+        }
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c < b) {
+            t = b;
+            b = c;
+            c = t;
+        }
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c > d) {
+            t = d;
+            d = c;
+            c = t;
+        }
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c < b) {
+            t = b;
+            b = c;
+            c = t;
+        }
+        
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c < a) {
+            t = a;
+            a = c;
+            c = t;
+        }
+
+        System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if (c > d) {
+            t = d;
+            d = c;
+            c = t;
+        }
+        
+        if(c>= a && c >= b && c <= d && c <= d) pass = true;
+        System.out.println("Median: " + c + " order: " + a + "," + b + "," + c +"," + d + "," + e );
+        if(pass) System.out.println("CORRECT");
+        else {
+            System.out.println("INCORRECT");
+            incorrect++;
+        }
+    }
+    System.out.println("Incorrect: " + incorrect);
    } 
 }
