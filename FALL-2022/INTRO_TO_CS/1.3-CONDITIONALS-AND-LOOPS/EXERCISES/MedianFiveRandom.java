51934a3d0a688d979de992b4229831a10d094d81
jgranick <jmgranick@gmail.com>
2022-08-13 07:26:09 -0400

    Median check w/ random numbers

--- FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/MedianFiveRandom.java
+++ FALL-2022/INTRO_TO_CS/1.3-CONDITIONALS-AND-LOOPS/EXERCISES/MedianFiveRandom.java
@@ -1,11 +1,21 @@
-public class MedianFive {
+public class MedianFiveRandom {
    public static void main(String[] args){
+    /* 
     int a = Integer.parseInt(args[0]);
     int b = Integer.parseInt(args[1]);
     int c = Integer.parseInt(args[2]);
     int d = Integer.parseInt(args[3]);
     int e = Integer.parseInt(args[4]);
+    */
+    int n = Integer.parseInt(args[0]);
+    for(int i = 0; i < n; i++ ){
+    
     int t;
+    int a = (int)(Math.random()*4.0 +1.0);
+    int b = (int)(Math.random()*4.0 +1.0);
+    int c = (int)(Math.random()*4.0 +1.0);
+    int d = (int)(Math.random()*4.0 +1.0);
+    int e = (int)(Math.random()*4.0 +1.0);
 
     System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
     if (a > b) {
@@ -26,30 +36,25 @@ public class MedianFive {
         b = t;
     }
     System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (c > d) {
-        t = c;
-        c = d;
-        d = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (c > e) {
-        t = c;
-        c = e;
-        e = t;
-    }
-    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (d > e) {
-        t = d;
+    if (e < d) {
+        t = e;
+        e = d;
         d = e;
-        e = t;
     }
-   /*  System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
-    if (b > c) {
-        t = c;
-        c = b;
-        b = t;
-    }*/
     System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+    if (e < c) {
+        t = e;
+        e = c;
+        c = t;
+    }
+    System.out.println("order: " + a + "," + b + "," + c +"," + d + "," + e );
+    if (d < c) {
+        t = d;
+        d = c;
+        c = t;
+    }
+
     System.out.println("Median: " + c + " order: " + a + "," + b + "," + c +"," + d + "," + e );
+}
    } 
 }
