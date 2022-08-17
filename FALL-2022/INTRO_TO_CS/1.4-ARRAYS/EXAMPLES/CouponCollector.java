d2ba2bfb4bfe642232ce6284feafdf09e1b7f4f1
jgranick <jmgranick@gmail.com>
2022-08-17 07:15:51 -0400

    (EQUIVALENT?) Example 1.4.2 CopuonCollector Problem BOOK IMPLEMENTATION pg 102

--- FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXAMPLES/CouponCollector.java
+++ FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXAMPLES/CouponCollector.java
@@ -3,16 +3,21 @@ public class CouponCollector {
       int n = Integer.parseInt(args[0]);
       boolean[] isCollected = new boolean[n];
       int count = 0;
+      int distinct = 0;
       
       for(int i = 0; i < n; i++)
       {
          //System.out.println(isCollected[i]);
-         while(isCollected[i] == false){
+         while(distinct < n){
             int r = (int)(Math.random()*(n));
             //System.out.println("r:" + r);
             //System.out.println(isCollected[i]);
             count++;
-            isCollected[r] = true;
+            if(!isCollected[r])
+            {
+               distinct++;
+               isCollected[r] = true;
+            }
             //System.out.println(isCollected[i]);
          }
          
