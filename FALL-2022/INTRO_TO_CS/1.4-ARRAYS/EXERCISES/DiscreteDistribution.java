af4c3486b2b02647fa4e9b0bb8eb6a052a8e60a2
jgranick <jmgranick@gmail.com>
2022-08-19 18:10:21 -0400

    1.4.12 DiscretDistribution

---
+++ FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXERCISES/DiscreteDistribution.java
@@ -0,0 +1,24 @@
+public class DiscreteDistribution {
+    public static void main(String[] args)
+    {
+    int n = args.length;
+    int[] freq = new int[n];
+    int total = 0;
+    for(int i = 0; i < n; i++)
+        {
+            freq[i] = Integer.parseInt(args[i]);
+            total+= freq[i];
+        }
+    
+    int r = (int)(Math.random()*total);
+    int event;
+    int sumTotal = 0;
+    for(int i = 0; sumTotal < r; i ++)
+    {   
+        sumTotal += freq[i];
+        event = i;
+        System.out.println("i: " + event + " count: " + freq[i] + " proportional prob: " + sumTotal + " random int r: " + r + " total prob: " + total);
+    }
+    }
+
+}
