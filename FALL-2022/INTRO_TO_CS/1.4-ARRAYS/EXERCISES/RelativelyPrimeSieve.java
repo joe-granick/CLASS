7bf9222598921f6bd0d4f703f9157e0d66660d4e
jgranick <jmgranick@gmail.com>
2022-08-22 18:18:43 -0400

    Problem 1.4.16 RelativePrimeSieve MOST CORRECT

--- FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXERCISES/RelativelyPrimeSieve.java
+++ FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXERCISES/RelativelyPrimeSieve.java
@@ -8,49 +8,36 @@ public class RelativelyPrimeSieve {
     {
         int n = Integer.parseInt(args[0]);
         boolean[][] isRelativelyPrime = new boolean[n+1][n+1];
-        int offset = n-1; //variable to prevent diagonal duplicates from being initialized to true
         char prime; //track T if relative prime and F otherwios for printing
         int steps = 0;
         
+        
         //initialize unique matrix combos of numbers to true to check for GCD
         //offset value is used to prevent diagonal mirrors of [i][j] so they can be skipped in evaluation
-        for(int i = 2; i < n+1; i++){
-            for(int j = 2; j < n-offset+2; j++)
-            {
-                isRelativelyPrime[i][j] = true;            
-            }
-            offset--;
+        for(int i = 1; i <=n; i++)
+        {
+            for(int j = 1; j <= n; j++) isRelativelyPrime[i][j] = true;            
         }
-        for (int i = 2; i <= n; i++){
-            for (int j = 2; j <= n; j++){
-                if(isRelativelyPrime[i][j]){
-                    for (int factor = 2; factor*i <= n; factor++)
+        for (int i = 2; i < n; i++){
+            for (int j = i; j < n; j++){
+                if (isRelativelyPrime[i][j])
+                {
+                    for (int f = 2; i*f <=n && j*f <= n; f++)
                     {
-                        if (factor*i <= n && factor*j <= n) isRelativelyPrime[factor*i][j*factor] = false;
-                        if(factor*j <= n) isRelativelyPrime[factor][j*factor] = false;
-                        if (factor*i <= n) isRelativelyPrime[factor*i][factor] = false;
-                        System.out.println("i: " + i + " j: " + j + " factor: " + factor);
+                        isRelativelyPrime[f][f*i] = false;
+                        isRelativelyPrime[f*i][f] = false;
+                        isRelativelyPrime[f][f*j] = false;
+                        isRelativelyPrime[f*j][f] = false;
+                        if (i*f != f*j)
+                        {
+                            isRelativelyPrime[i*f][j*f] = false;
+                            isRelativelyPrime[j*f][i*f] = false;
+                        }
                         steps++;
                     }
                 }
             }
         }
-        // set multiples to false
-        /*for(int i = 2; i < n+1; i++){
-            for(int j = 2; j < n+1; j++){
-                if (isRelativelyPrime[i][j]){ 
-                for (int k = 2; (k*i < n+1) && (k*j < n+1); k++){
-                    isRelativelyPrime[i*k][j*k] = false;
-                    isRelativelyPrime[i*k][k] = false;
-                    isRelativelyPrime[k][j*k] = false;
-                    steps++;
-                }
-                isRelativelyPrime[j][j] = false;
-                }
-                isRelativelyPrime[i][i] = false;         
-            }
-        }
-*/
         int relativePrimeCount = 0;
         for (int j = 0; j < n+1; j++) System.out.print(j + " "); // Column labels
         System.out.println();   
@@ -62,13 +49,14 @@ public class RelativelyPrimeSieve {
                     relativePrimeCount++;
                 }
                 else                  prime = 'F';
-                if(i >= j) System.out.print(prime + " ");
+                System.out.print(prime + " ");
             }
             System.out.println();
             
+            
         }
-        System.out.println("Relative Primes: " + relativePrimeCount);
         System.out.println("steps: " + steps);
+        System.out.println("relative primes: " + relativePrimeCount);
     }
     
 }
