e743644d34be14318ec73fe82bf675f6c12cd52a
jgranick <jmgranick@gmail.com>
2022-08-24 07:43:44 -0400

    Problem 1.4.23 longest plateau FINISHED pg 121

--- FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXERCISES/LongestPlateau.java
+++ FALL-2022/INTRO_TO_CS/1.4-ARRAYS/EXERCISES/LongestPlateau.java
@@ -1,34 +1,43 @@
 public class LongestPlateau {
+    /*find longest sequence of equivalent number where number preceding and following
+     * are less than the number of the sequence
+    */
+
     public static void main(String[] args)
     {
         int longestPlateau = 0;
         int maxStart = 0;
         int maxEnd = 0;
-        for(int i = 1; i < args.length-2; i++)
+        for(int i = 1; i < args.length-1; i++)
         {
             int count = 0;
             int a = Integer.parseInt(args[i-1]);
             int b = Integer.parseInt(args[i]);
             int c = Integer.parseInt(args[i+1]);
-            
-            while(c == b && count < args.length-1){
+            //continues count at start of i while each succesive number is equal
+            while(c == b && (i+1+count) < args.length-1){
                 count++;
-                a = Integer.parseInt(args[i-1 + count]);
-                b = Integer.parseInt(args[i + count]);
-                c = Integer.parseInt(args[i+1 + count]);
-                System.out.print(i+count + " ");
+                a = Integer.parseInt(args[i-1+count]);
+                b = Integer.parseInt(args[i+count]);
+                c = Integer.parseInt(args[i+1+count]);
+                //System.out.println(a + " " + c);
             }
-            if(count > longestPlateau) {
-                longestPlateau += count;
-                maxEnd = c;
+            count++;
+                //count++; //needed to include last number in sequence
+            // checks that count > current longest and 
+            if(count > longestPlateau && c < b) {
+                longestPlateau = count;
+                maxEnd = i+count-1;
                 maxStart = i;
-                System.out.println(count);
+                //System.out.println(count);
             }
-            System.out.println(longestPlateau);
+            //System.out.println(longestPlateau);
         }
 
         
-        System.out.println("longest sequence: " + longestPlateau + " starting at : " + maxStart + " " + args[maxStart] + " ending at " + maxEnd + " " + args[maxEnd]);
+        System.out.println("longest sequence: " + longestPlateau + " starting at : " + maxStart  + " ending at " + maxEnd);
+        System.out.println();
+        for(int i = maxStart-1; i <= maxEnd+1; i++) System.out.print(args[i]);
     }
     
 }
