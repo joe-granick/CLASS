535430036d86f9d99a68f71607bf27a7da7a6124
jgranick <jmgranick@gmail.com>
2022-09-11 15:22:29 -0400

    1.5.9 Dragon curves fed into self referencing program to create fractal pattern (recursion precursor?) pg 162-163

---
+++ FALL-2022/INTRO_TO_CS/1.5 INPUT-AND-OUTPUT/HOMEWORK/Dragon.java
@@ -0,0 +1,13 @@
+public class Dragon 
+{
+    public static void main(String[] args)
+    {
+        String dragon = StdIn.readString();
+        String nogard =StdIn.readString();
+        StdOut.print(dragon + "L" + nogard);
+        StdOut.print(" ");
+        StdOut.print(dragon + "R" +nogard);
+        StdOut.println();
+    }
+    
+}
