3bdefb552952b46ff4c8953c9f7facaae7ed3db0
jgranick <jmgranick@gmail.com>
2022-09-14 14:00:55 -0400

    HW4.2 ARRAYS RURottentomatoes, matrix constructed still need colsum and index of max Due: 10-21-22

---
+++ FALL-2022/INTRO_TO_CS/HOMEWORK/4-ARRAYS/FindDuplicate.java
@@ -0,0 +1,49 @@
+/*************************************************************************
+ *  Compilation:  javac FindDuplicate.java
+ *  Execution:    java FindDuplicate
+ *
+ *  @author:
+ *
+ * FindDuplicate that reads n integer arguments from the command line 
+ * into an integer array of length n, where each value is between is 1 and n, 
+ * and displays true if there are any duplicate values, false otherwise.
+ *
+ *  % java FindDuplicate 10 8 5 4 1 3 6 7 9
+ *  false
+ *
+ *  % java FindDuplicate 4 5 2 1 
+ *  true
+ *************************************************************************/
+
+public class FindDuplicate {
+
+    public static void main(String[] args) {
+		int n = args.length;
+		int[] dupVal = new int[n];
+		int max = 0;
+		int num;
+
+		for (int i = 0; i < n; i++)
+		{
+			num = Integer.parseInt(args[i]);
+			dupVal[i] = num;
+			if(num > max) max = num;
+		}
+
+		int[] hasDup = new int[max+1];
+		boolean duplicate = false;
+		for(int i = 0; i < n; i++)
+		{
+			if(hasDup[dupVal[i]] == 1)
+			{
+				duplicate = true;
+				break;
+			}
+			else hasDup[dupVal[i]] = 1;
+		}
+		System.out.println(duplicate);
+		System.out.println(max);
+		System.out.println(hasDup.length);
+		//WRITE YOUR CODE HERE
+	}
+}
