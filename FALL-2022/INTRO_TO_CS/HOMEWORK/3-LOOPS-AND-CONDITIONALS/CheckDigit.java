54536f519d05ef42580bd3500b89f632a339bd06
jgranick <jmgranick@gmail.com>
2022-09-14 09:35:45 -0400

    HW3.2 CONDITIONALS AND LOOPS CheckDigit Due: 10-14-22

--- FALL-2022/INTRO_TO_CS/HOMEWORK/3-LOOPS-AND-CONDITIONALS/CheckDigit.java
+++ FALL-2022/INTRO_TO_CS/HOMEWORK/3-LOOPS-AND-CONDITIONALS/CheckDigit.java
@@ -25,5 +25,23 @@ public class CheckDigit {
     public static void main (String[] args) {
 
         // WRITE YOUR CODE HERE
+        long digit = Long.parseLong(args[0]);
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i = 0; i < 13; i++){
+            if(i%2 == 0) sum1 += digit%10; //isolate rightmost digit evens
+            else         sum2 += digit%10; //isolate rightmost digit odds
+                         digit = digit/10; //remove rightmost digit     
+            //System.out.println("Sum 1: " + sum1 + " Sum 2: " +sum2);
+        }
+        sum1 = sum1%10; //isolate rightmost digit of evens sum
+        //isolate rightmost digit of odds sum multiply by 3
+        sum2 = ((sum2%10)*3)%10; 
+        
+        int sum3 = sum1 + sum2; //sum check digits of both sums
+        int checkDigit =sum3%10; //isolate rightmost check digit
+        //System.out.println("Digit 1: " + sum1 + " Digit 2: " + sum2 + " Check Digit: " + checkDigit);
+        System.out.println(checkDigit);
+
     }
 }
