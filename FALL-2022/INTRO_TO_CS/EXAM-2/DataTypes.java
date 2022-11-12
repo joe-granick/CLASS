3105392e045c23c435327c9bb525ad3aec73e2af
Joseph Granick <jgranick@pop-os.localdomain>
2022-11-12 04:41:21 -0500

    Sierpinski tweaks

---
+++ FALL-2022/INTRO_TO_CS/EXAM-2/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+    System.out.println("System.out.println((3 + 4.5))                   :  " + (3+4.5));
+    System.out.println("System.out.println( 3 + 4.5 )                   :  " + 3+4.5);
+    System.out.println("Math.pow(5/2,2)                                :  " + Math.pow(5/2,2));
+    System.out.println("Math.pow(5.0/2,2)                              :  " + Math.pow(5.0/2,2));
+    System.out.println("Math.min(2,4))                                 :  " + Math.min(2,4));
+    System.out.println("Math.min(2,4))                                 :  " + Math.min(2.0,4.0));
+    System.out.println("Math.ceil(5.2)                                 :  " + Math.ceil(5.2));
+    System.out.println("Math.max(19-5+Math.random(), Math.ceil(5.1*3))): " + Math.max(19-5+Math.random(), Math.ceil(5.1*3)));
+    
+    }
+}
