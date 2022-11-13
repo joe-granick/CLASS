3cf0cfac8eef3724037320eeb203dc1204731987
jgranick <jmgranick@gmail.com>
2022-11-13 18:52:33 -0500

    Sierpinski old

--- FALL-2022/INTRO_TO_CS/WEEK-10/HOMEWORK-RECURSION/cs111-a7/Sierpinski.java
+++ FALL-2022/INTRO_TO_CS/WEEK-10/HOMEWORK-RECURSION/cs111-a7/Sierpinski.java
@@ -16,26 +16,21 @@ public class Sierpinski {
     public static double height(double length) {
 
 	// WRITE YOUR CODE HERE
-    double height = (Math.sqrt(3.0)/2)*length;
-    return height;
+        double height = (Math.sqrt(3.0)/2)*length;
+        return height;
     }
 
     // Draws a filled equilateral triangle whose bottom vertex is (x, y) 
     // of the specified side length. 
     public static void filledTriangle(double x, double y, double length) {
-
-    double h = height(length);
-	
-    
-    xCoord[0] = x - length/2;
-    xCoord[1] = x           ;
-    xCoord[2] = x + length/2;
-
-    yCoord[0] = y +      h;
-    yCoord[1] =          y;
-    yCoord[2] = y +      h;
-    StdDraw.filledPolygon(xCoord, yCoord);
-
+        double h = height(length);
+        xCoord[0] = x - length/2;
+        xCoord[1] = x           ;
+        xCoord[2] = x + length/2;
+        yCoord[0] = y +      h;
+        yCoord[1] =          y;
+        yCoord[2] = y +      h;
+        StdDraw.filledPolygon(xCoord, yCoord);
     }
 
     // Draws a Sierpinski triangle of order n, such that the largest filled 
@@ -43,39 +38,26 @@ public class Sierpinski {
     public static void sierpinski(int n, double x, double y, double length) {
 
 	// WRITE YOUR CODE HERE
-    if (n == 0)  return;
-    
-    filledTriangle(x, y, length);
-    sierpinski(n-1, x-length/2, y, length/2);
-    sierpinski(n-1, x, y+height(length), length/2);
-    sierpinski(n-1, x+length/2, y, length/2);
-
+        if (n == 0)  return;
+        filledTriangle(x, y, length);
+        sierpinski(n-1, x-length/2, y, length/2);
+        sierpinski(n-1, x, y+height(length), length/2);
+        sierpinski(n-1, x+length/2, y, length/2);
     }
 
     // Takes an integer command-line argument n; 
     // draws the outline of an equilateral triangle (pointed upwards) of length 1; 
     // whose bottom-left vertex is (0, 0) and bottom-right vertex is (1, 0); and 
     // draws a Sierpinski triangle of order n that fits snugly inside the outline. 
-    public static void main(String[] args) {
-
-	
-    int n = Integer.parseInt(args[0]);
-
-    double x = 0.5;
-    double y = 0.0;
-    double l = 0.5;
-    
-    double h = height(1.0);
-    StdDraw.line(0, 0, 1, 0);
-    StdDraw.line(0, 0, 0.5, h);
-    StdDraw.line(1, 0, 0.5, h);
-    sierpinski(n, x, y, l);
-    // filledTriangle(0.5, 0.0, 0.5);
-    // filledTriangle(0.25, 0.0, 0.25);
-    // filledTriangle(0.75, 0.0, 0.25);
-    // filledTriangle(0.5,     h/2, 0.25);
-    
-    
-
+        public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        double x= 0.5;
+        double y = 0.0;
+        double l = 0.5;
+        double h = height(1.0);
+        StdDraw.line(0, 0, 1, 0);
+        StdDraw.line(0, 0, 0.5, h);
+        StdDraw.line(1, 0, 0.5, h);
+        sierpinski(n, x, y, l);
     }
 }
